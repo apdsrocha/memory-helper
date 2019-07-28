40a138890869fcdf8fb29f466b710ed027210bf9
Ana Rocha <apdsrocha@gmail.com>
2019-07-28 10:33:28 -0300

    added moments.js and frozen-moment library to add the date in the entries created

--- src/Entry/entryList.jsx
+++ src/Entry/entryList.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import Moment from 'moment'
+import freeze from 'frozen-moment'
 
 import EntryWeather from '../Entry/entryWeather'
+
+
+let time = Moment().format('LLL')
+
+
 
 export default props => {
     let locationList = props.locationList || []
     let list = props.list || []
-    
+    let frozenTime = String(freeze(time))
     let infoCard = locationList.map((value, index) =>
-        <div key={'weather'+index} >
-       <p key={'place'+index} className="location-text">{value}</p> <p key={'content'+index} className="description-text">{list[index]}</p>
-
+        <div key={'card'+index} >
+     <p key={'content'+index} className="description-text">{list[index]}</p>
        <EntryWeather userLat={props.userLat} userLong={props.userLong} location={value}/>
+       <p>{frozenTime.slice(0, -17)}</p>
        </div>
        );
 
--- src/Entry/entryWeather.jsx
+++ src/Entry/entryWeather.jsx
@@ -8,13 +8,15 @@ class EntryWeather extends Component {
             city: '',
             temperature: '',
             countryId: '',
+            icon: '',
+            condition: '',
             location: this.props.location,
             userLat: this.props.userLat,
             userLong: this.props.userLong
         };
     }
     componentDidMount() {
-    const KEY = 'YOUR KEY'
+    const KEY = 'ab4799176cd5cf774957846b3fde04b1'
     const ADRESSBYLATLONG = `https://api.openweathermap.org/data/2.5/weather?lat=${this.props.userLat}&lon=${this.props.userLong}&units=metric&APPID=${KEY}`
     const ADDRESSBYCITY = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.location}&units=metric&APPID=${KEY}`
     const FETCHCALL = this.props.userLat && this.props.userLong ? ADRESSBYLATLONG : ADDRESSBYCITY
@@ -25,8 +27,11 @@ class EntryWeather extends Component {
             let city = data.name
             let temperature = data.main.temp
             let countryId = data.sys.country
+            let icon = data.weather[0]['icon']
+            let condition = data.weather[0]['main']
             this.setState({city: city});
-            this.setState({temperature: temperature});
+            this.setState({icon: icon});
+            this.setState({temperature: Math.round(temperature)});
             this.setState({countryId: countryId});
         })
     }
@@ -34,7 +39,9 @@ class EntryWeather extends Component {
         return (
         <div>
             <p>{this.state.city} - {this.state.countryId}</p>
-            <p>{this.state.temperature}</p>
+            <img src={'http://openweathermap.org/img/wn/' + this.state.icon + '.png'} />
+            <p>{this.state.condition}</p>
+            <p>{this.state.temperature} °C</p>
         </div>
         )
     }
