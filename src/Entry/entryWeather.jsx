6cafb160190370b8f8118781541a481f53946a4b
Ana Rocha <apdsrocha@gmail.com>
2019-07-30 18:59:02 -0300

    no longer using openweather icons, traded in for custom icons

--- src/Entry/entryWeather.jsx
+++ src/Entry/entryWeather.jsx
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
 
+import Thunderstom from '../assets/img/thunderstorm.svg'
+import Drizzle from '../assets/img/drizzle.svg'
+import Rain from '../assets/img/rain.svg'
+import Snow from '../assets/img/snow.svg'
+import Tornado from '../assets/img/tornado.svg'
+import Clear from '../assets/img/clear.svg'
+import Clouds from '../assets/img/cloud.svg'
+import Else from '../assets/img/else.svg'
 
 class EntryWeather extends Component {
     constructor(props) {
@@ -8,6 +16,7 @@ class EntryWeather extends Component {
             city: '',
             temperature: '',
             countryId: '',
+            condition: '',
             icon: '',
             error: null,
             location: this.props.location,
@@ -15,7 +24,8 @@ class EntryWeather extends Component {
             userLong: this.props.userLong
         };
     }
-    componentDidMount() {
+
+    componentWillMount() {
     const KEY = 'YOUR KEY'
     const ADRESSBYLATLONG = `https://api.openweathermap.org/data/2.5/weather?lat=${this.props.userLat}&lon=${this.props.userLong}&units=metric&APPID=${KEY}`
     const ADDRESSBYCITY = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.location}&units=metric&APPID=${KEY}`
@@ -30,21 +40,45 @@ class EntryWeather extends Component {
             let city = data.name
             let temperature = data.main.temp
             let countryId = data.sys.country
-            let icon = data.weather[0]['icon']
+            let condition = data.weather[0]['main']
+
             this.setState({city: city});
             this.setState({temperature: temperature});
             this.setState({countryId: countryId});
-            this.setState({icon: icon});
+            this.setState({condition: condition});
+
         }).catch((error) => {
                 this.setState({error: error});
         });
     }
-    render() {
-        return ( 
+    
+render() {
+    return ( 
         <div>
             { this.state.error !== null ? <h4>Invalid city, try again.</h4> : 
             <div className="weather"> 
-            <img alt="icon" src={'http://openweathermap.org/img/wn/' + this.state.icon + '@2x.png'} />
+            <div className="img-icon">
+            {(() => {
+                switch(this.state.condition) {
+                case 'Thunderstorm':
+                    return <img alt="icon representing thunderstorms" src={Thunderstom} />;
+                case 'Drizzle':
+                    return <img alt="icon representing drizzle"src={Drizzle} />;
+                case 'Rain':
+                    return <img alt="icon representing rain"src={Rain} />;
+                case 'Snow':
+                    return <img alt="icon representing snow"src={Snow} />;
+                case 'Tornado':
+                    return <img alt="icon representing tornado"src={Tornado} />;
+                case 'Clear':
+                    return <img alt="icon representing clear skies"src={Clear} />;
+                case 'Clouds':
+                    return <img alt="icon representing cloudy weather"src={Clouds} />;
+                default:
+                    return <img alt="icon representing weather"src={Else} />;;
+                }
+            })()}
+            </div>
             <p className="temperature">{Math.round(this.state.temperature)} °C</p>
             <p className="city">{this.state.city} - {this.state.countryId}</p>
             </div>
